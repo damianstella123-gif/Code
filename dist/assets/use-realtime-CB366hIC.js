e2513e4deaa0e970e45598baaba7996cb47ded5b
damianstella123-gif <damian.stella123@gmail.com>
2026-07-03 10:22:10 +0200

    Updated index.html

--- dist/assets/use-realtime-CB366hIC.js
+++
@@ -1,1 +0,0 @@
-import{a4 as c,a7 as a}from"./index-ClIew7Ob.js";let u=0;function l(e,n){const s=c.useRef(n);s.current=n,c.useEffect(()=>{const r=++u,t=a.channel(`realtime-${e}-${r}`).on("postgres_changes",{event:"*",schema:"public",table:e},()=>{s.current()}).subscribe();return()=>{a.removeChannel(t)}},[e])}export{l as u};
