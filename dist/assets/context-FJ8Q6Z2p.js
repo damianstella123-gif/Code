2159ceeb6964cd235ce24cbcca3cdade74f707c1
damianstella123-gif <damian.stella123@gmail.com>
2026-07-07 13:20:50 +0200

    Publish application

---
+++ dist/assets/context-FJ8Q6Z2p.js
@@ -0,0 +1,1 @@
+let t={page:"dashboard"};function n(e){t={...t,...e}}export{n as s};
---
+++ dist/assets/use-realtime-CO2GS1Na.js
@@ -0,0 +1,1 @@
+import{ah as c,al as a}from"./index-Cp_YB1gd.js";let u=0;function l(e,n){const s=c.useRef(n);s.current=n,c.useEffect(()=>{const r=++u,t=a.channel(`realtime-${e}-${r}`).on("postgres_changes",{event:"*",schema:"public",table:e},()=>{s.current()}).subscribe();return()=>{a.removeChannel(t)}},[e])}export{l as u};
