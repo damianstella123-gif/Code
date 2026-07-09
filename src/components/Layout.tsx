c17587d9d86915fe6b7e273614d23f3a32fcb60f
damianstella123-gif <damian.stella123@gmail.com>
2026-07-09 08:56:23 +0200

    Update shell layout for centering

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -692,9 +692,9 @@ export default function Layout({ children }: { children: ReactNode }) {
           <div className="shell-light-secondary" />
         </div>
         <Sidebar open={sidebarOpen} setOpen={setSidebarOpen} />
-        <div className="shell-main" style={{ alignItems: 'center' }}>
+        <div className="shell-main">
           <Topbar setOpen={setSidebarOpen} />
-          <main className="shell-content pb-mobile-nav" style={{ alignSelf: 'center', width: '100%' }}>
+          <main className="shell-content pb-mobile-nav" style={{ alignSelf: 'center', width: '100%', maxWidth: '1280px', marginLeft: 'auto', marginRight: 'auto' }}>
             {children}
           </main>
         </div>
